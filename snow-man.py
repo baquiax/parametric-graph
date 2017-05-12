b4c369ae6a8704ac13eadab3993d97e4ebcb3898
Alex <alex@yalochat.com>
2017-05-12 00:35:02 -0600

    face & body

--- snow-man.py
+++ snow-man.py
@@ -16,16 +16,33 @@ r = 0.9
 h = 6
 face += parametric_plot3d([6 + u, ((h-u)/h) * r*sin(v), -0.5 + ((h-u)/h) * r*cos(v) ], (u, 0, h),(v, 0, 2*pi), color='orange')
 
+##face + mouth (parabola)
+face += parametric_plot3d([v, u,-3 + .2*pow(u ,2)], (u, -2, 2), (v, 4, 5.3), color='brown');
+
 #body (Ellipsoid)
-a = 10
-b = 10
-c = 12
+##upper torax
+a = 9
+b = 9
+c = 8
 
 x = 0
 y = 0
-z = -16
+z = -12
 
 body = parametric_plot3d([x + a*cos(u)*sin(v), y + b*sin(u)*sin(v), z + c*cos(v)], (u, 0, 2*pi), (v, 0, pi), color='white')
+
+##lower torax
+a = 12
+b = 12
+c = 11
+
+x = 0
+y = 0
+z = -25
+
+body += parametric_plot3d([x + a*cos(u)*sin(v), y + b*sin(u)*sin(v), z + c*cos(v)], (u, 0, 2*pi), (v, 0, pi), color='white')
+
+
 
 rabit = body + face
 rabit.show(aspect_ratio=[1,1,1])
